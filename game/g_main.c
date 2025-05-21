292d283c21c791841eff53b80f7ca5d025155e4d
Jakendary <176844671+Jakendary@users.noreply.github.com>
2025-05-21 19:26:38 +0800

    release: update version to 3.2.0 for new release build

--- game/g_main.c
+++ game/g_main.c
@@ -44,7 +44,7 @@ const char *verMods =
 
 #else
 
-#define LUGORMODVERSION_CORE "v3.1.1"
+#define LUGORMODVERSION_CORE "v3.2.0"
 #ifdef LMD_EXPERIMENTAL
 #define LUGORMODVERSION LUGORMODVERSION_CORE" Alpha"
 #else

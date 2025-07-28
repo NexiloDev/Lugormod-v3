688f95a466f2f394a5d63e74bdda1872d3ef12ea
Milo Hehmsoth <mhehmsoth@gmail.com>
2025-07-27 22:16:05 -0500

    broke a brace

--- game/g_misc.c
+++ game/g_misc.c
@@ -50,7 +50,7 @@ const entityInfoData_t SP_info_camp_spawnflags[] = {
 	{NULL, NULL}
 };
 const entityInfoData_t SP_info_camp_keys[] = {
-	 "make the trigger target this value for the entity to be used"},
+	{"targetname", "make the trigger target this value for the entity to be used"},
 	{NULL, NULL}
 };
 const entityInfo_t SP_info_camp_info = {

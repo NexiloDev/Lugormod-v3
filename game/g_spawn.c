70067654c2113cb71ae251a5c9fa637795f4821b
Milo Hehmsoth <mhehmsoth@gmail.com>
2025-07-27 23:11:09 -0500

    somehow missed brackets again

--- game/g_spawn.c
+++ game/g_spawn.c
@@ -847,9 +847,9 @@ spawn_t	spawnInitValues[] = {
 	{"info_player_intermission", SP_info_player_intermission, Logical_True, &info_player_intermission_info},
 	{"info_player_intermission_red", SP_info_player_intermission_red, Logical_True, &info_player_intermission_red_info},
 	{"info_player_intermission_blue", SP_info_player_intermission_blue, Logical_True, &info_player_intermission_blue_info},
-	{"info_jedimaster_start", SP_info_jedimaster_start, Logical_True, &info_jedimaster_start_info,
+	{"info_jedimaster_start", SP_info_jedimaster_start, Logical_True, &info_jedimaster_start_info},
 	{"info_player_start_red", SP_info_player_start_red, Logical_True, &info_player_start_red_info},
-	{"info_player_start_blue", SP_info_player_start_blue, Logical_True}, &info_player_start_blue_info,
+	{"info_player_start_blue", SP_info_player_start_blue, Logical_True, &info_player_start_blue_info},
 	{"info_null", SP_info_null, Logical_True, &SP_info_null_info},
 	{"info_notnull", SP_info_notnull, Logical_True, &SP_info_notnull_info},		// use target_position instead
 	{"info_camp", SP_info_camp, Logical_True, &SP_info_camp_info},

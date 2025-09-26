b8888ae69405c6b90cd0e5573b06685b797721de
lumayaa <89487897+lumayaa@users.noreply.github.com>
2025-09-26 16:49:40 +0200

    Added TaystJKInfo.

--- game/g_main.c
+++ game/g_main.c
@@ -202,6 +202,7 @@ vmCvar_t	g_useWhileThrowing;
 vmCvar_t	g_RMG;
 
 vmCvar_t	g_svfps;
+vmCvar_t	taystJKinfo;
 
 vmCvar_t	g_forceRegenTime;
 vmCvar_t	g_spawnInvulnerability;
@@ -716,6 +717,8 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_RMG, "RMG", "0", 0, 0, qtrue  },
 
 	{ &g_svfps, "sv_fps", "20", 0, 0, qtrue },
+
+	{ &taystJKinfo, "taystJKinfo", "0", CVAR_SERVERINFO|CVAR_ROM, 0, qfalse },
 
 	{ &g_forceRegenTime, "g_forceRegenTime", "200", CVAR_SERVERINFO | CVAR_ARCHIVE, 0, qtrue  },
 
@@ -1690,6 +1693,14 @@ void Lmd_Startup(void);
 qboolean AllForceDisabled(int force);
 void InitializeSpawnTable();
 
+void G_SetTaystJKFlags( void)
+{
+	int taystJKFeatures = 0;
+	taystJKFeatures |= TAYSTJK_INFO_RGBSABERS;
+	taystJKFeatures |= TAYSTJK_INFO_BLACKSABERS;
+	trap_Cvar_Set("taystJKinfo", va("%d", taystJKFeatures));
+}
+
 void G_SiegeRegisterWeaponsAndHoldables(int team); //Lugormod GT_BATTLE_GROUND
 void G_InitGame( int levelTime, int randomSeed, int restart ) {
 	int					i;
@@ -1703,6 +1714,7 @@ void G_InitGame( int levelTime, int randomSeed, int restart ) {
 	}
 #endif
 
+	G_SetTaystJKFlags();
 	G_InitMemory();
 
 	//RoboPhred:

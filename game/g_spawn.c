50306d538462957eeda38c8d6c7a1ac963635c90
lumayaa <89487897+lumayaa@users.noreply.github.com>
2025-11-08 14:04:09 +0100

    Init.

--- game/g_spawn.c
+++ game/g_spawn.c
@@ -446,6 +446,8 @@ void SP_target_location (gentity_t *ent);
 extern entityInfo_t target_location_info;
 void SP_target_counter (gentity_t *self);
 extern entityInfo_t target_counter_info;
+void SP_target_doorstate (gentity_t *self);
+extern entityInfo_t target_doorstate_info;
 void SP_target_random (gentity_t *self);
 extern entityInfo_t target_random_info;
 void SP_target_scriptrunner( gentity_t *self );
@@ -946,6 +948,7 @@ spawn_t	spawnInitValues[] = {
 	{"target_position", SP_target_position, Logical_True, &target_position_info},
 	{"target_location", SP_target_location, Logical_True, &target_location_info},
 	{"target_counter", SP_target_counter, Logical_True, &target_counter_info},
+	{"target_doorstate", SP_target_doorstate, Logical_True, &target_doorstate_info},
 	{"target_random", SP_target_random, Logical_True, &target_random_info},
 	{"target_scriptrunner", SP_target_scriptrunner, qfalse, &target_scriptrunner_info},
 	{"target_interest", SP_target_interest, Logical_True},
